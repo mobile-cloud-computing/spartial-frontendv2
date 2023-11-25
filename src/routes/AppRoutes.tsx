fdd5946b87065e2fb5b0621671f46657dbae236c
toluwani <toluwani@ut.ee>
2023-11-25 19:13:25 +0200

    Refactor: Organize and configure application routes in AppRoutes component

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,37 +1,34 @@
 import React from 'react';
-import {Routes, Route} from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import LoginWidget from "../customComponents/auth/LoginWidget";
 import { LoginCallback } from "@okta/okta-react";
 import ProtectedRoute from "../customComponents/util/ProtectedRoute";
 import ProtectedComponent from "../views/ProtectedComponent";
 import ModelPage from "../pages/home/ModelPage";
 import Dashboard from "../pages/home/Dashboard";
+import BuildACModelForm from "../customComponents/models/BuilldACModelForms";
+import NoMatchComponent from "../customComponents/NoMatchComponent";
+
+const routeConfig = [
+    { path: '/', element: <Dashboard /> },
+    { path: '/build/ad', element: <ModelPage /> },
+    { path: '/build/ac', element: <BuildACModelForm /> },
+    { path: '/login', element: <LoginWidget />, isProtected: false },
+    { path: '/login/callback', element: <LoginCallback />, isProtected: false },
+    { path: '/protected', element: <ProtectedComponent /> },
+    { path: '*', element: <NoMatchComponent />, isProtected: false } // No match route
+];
+
 const AppRoutes = () => {
     return (
         <Routes>
-            <Route path='/'
-                   element={
-                        <ProtectedRoute>
-                            <Dashboard />
-                        </ProtectedRoute>}
-            />
-            <Route path='/models'
-                   element={
-                        <ProtectedRoute>
-                            <ModelPage />
-                        </ProtectedRoute>}
-            />
-            <Route path='/login' element={<LoginWidget/>}/>
-            <Route path='/login/callback' element={<LoginCallback/>}/>
-            <Route
-                path='/protected'
-                element={
-                    <ProtectedRoute>
-                        <ProtectedComponent/>
-                    </ProtectedRoute>
-                }
-            />
-            <Route path="*" element="No match"/>
+            {routeConfig.map(({ path, element, isProtected = true }) => (
+                <Route
+                    key={path}
+                    path={path}
+                    element={isProtected ? <ProtectedRoute>{element}</ProtectedRoute> : element}
+                />
+            ))}
         </Routes>
     );
 };
