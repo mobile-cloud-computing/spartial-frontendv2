fdcb48ef057789d8f20670f6ce8bce82f880591a
toluwani <toluwani@ut.ee>
2023-11-25 19:12:29 +0200

    Feature: Created form handling and UI BuildACModelForm component

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,8 @@
 export const SERVER_HOST = "localhost";
 export const SERVER_PORT = 31057;
 export const SERVER_URL = `http://${SERVER_HOST}:${SERVER_PORT}`;
+
+
+// BuildACPage.tsx
+export const AI_MODEL_TYPES = ['Neural Network', 'XGBoost', 'LightGBM']
+export const FEATURE_OPTIONS =  ['Raw']
