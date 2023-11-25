2b076cc25e7f6a6b7674c4d759f932907b2fad95
toluwani <toluwani@ut.ee>
2023-11-25 19:33:51 +0200

    refactor: Centralize error handling and API request logic, making it easier to manage and modify in the future.

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,76 +1,65 @@
-import axios from "axios";
+import axios, {AxiosResponse} from "axios";
 import { SERVER_URL } from "../constants";
-import {BuildStatusInterface, MMTStatusInterface} from "../context/context";
+import {
+    DatasetType,
+    TParamType,
+    BuildStatusType,
+    MMTStatusInterface
+} from "../types/types";
 
 
 interface Option {
-
     reports:  string
     isRunning: string
     lastBuildAt: string
     lastBuildId: string
     config: any
-
 }
+
+export interface Ac {
+    datasets: string[]
+}
+
+async function makeApiRequest<T>(url: string, method: 'get' | 'post' = 'get', payload?: any): Promise<T | null> {
+    try {
+        const response: AxiosResponse<T> = method === 'post'
+            ? await axios.post(url, payload)
+            : await axios.get(url);
+        return response.data;
+    } catch (error) {
+        console.error(`Error in API request to ${url}:`, error);
+        return null;
+    }
+}
+
 export const requestAllReports = async () => {
-    const url = `${SERVER_URL}/api/reports`;
-    try {
-
-        const response = await axios.get<Option[]>(url);
-        return response.data;
-    } catch (error) {
-        console.error('Error fetching reports:', error);
-        return [];
-    }
+    return makeApiRequest<Option[]>(`${SERVER_URL}/api/reports`);
 };
-
 export const requestMMTStatus = async () => {
-    const url = `${SERVER_URL}/api/mmt`;
-    try {
-        const response = await axios.get<MMTStatusInterface>(url)
-        return response.data;
-    }
-    catch (error) {
-        console.error('Error fetching reports:', error);
-        return [];
-    }
+    return makeApiRequest<MMTStatusInterface>(`${SERVER_URL}/api/mmt`);
+};
+export const requestBuildADModel = async (
+    datasets: DatasetType,
+    ratio: number,
+    params: TParamType
+): Promise<BuildStatusType | null> => {
+    const buildConfig = { datasets, "training_ratio": ratio, "training_parameters": params };
+    return makeApiRequest<BuildStatusType>(`${SERVER_URL}/api/build`, 'post', { buildConfig });
 };
 
-export const requestBuildModel = async (datasets: any, ratio: any, params: any) => {
-    const url = `${SERVER_URL}/api/build`;
-    const buildConfig = {
-        "datasets": datasets,
-        "training_ratio": ratio,
-        "training_parameters": params,
-    };
-
-    try {
-        const response = await axios.post<Option>(url, {buildConfig});
-        return response.data;
-    } catch (error) {
-        console.error('Error:', error);
-        throw error;
-    }
+export const requestDatasetAC = async () => {
+    return makeApiRequest<Ac>(`${SERVER_URL}/api/ac/datasets`);
 };
-
 
 export const requestBuildStatusAC = async () => {
-    const url = `${SERVER_URL}/api/ac/build`;
-
-    try {
-        const response = await axios.get<BuildStatusInterface>(url)
-
-        if (Array.isArray(response) ?? response == null ) {
-            // Handle the array case (never[] should technically never happen)
-            console.error("Unexpected type: never[]");
-            return;
-        }
-
-        return response.data.buildStatus
-
-    } catch (error) {
-        console.error('Error fetching reports:', error);
-        return [];
-    }
+    const response = await makeApiRequest<any>(`${SERVER_URL}/api/ac/build`);
+    return response ? response.buildStatus : null;
 };
+
+export const requestBuildACModel = async (modelType: any, dataset: any, featuresList: any, trainingRatio: any) => {
+    const buildACConfig = { modelType, dataset, featuresList, trainingRatio };
+    return makeApiRequest<any>(`${SERVER_URL}/api/ac/build`, 'post', { buildACConfig });
+};
+
+
 
