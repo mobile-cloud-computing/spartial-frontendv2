e77084d338301ab431b81c0e20b9ea00c0b96a05
toluwani <toluwani@ut.ee>
2023-12-06 11:16:10 +0200

    Update SERVER_HOST to use a service domain.

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,5 @@
-export const SERVER_HOST = "localhost";
+//export const SERVER_HOST = "localhost";
+export const SERVER_HOST = "193.40.154.245";
 export const SERVER_PORT = 31057;
 export const SERVER_URL = `http://${SERVER_HOST}:${SERVER_PORT}`;
 
