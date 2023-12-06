ee289aef07ab055290c2f0ca743a3b2c33584a92
toluwani <toluwani@ut.ee>
2023-12-06 14:02:59 +0200

    Refactor: Added demo dashboard

---
+++ src/pages/home/Dashboard.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+const Dashboard: React.FC = () => {
+    return (
+        <>
+            <div className="container my-4">
+                <h1>Dashboard Page</h1>
+                <p>This is the Dashboard.</p>
+            </div>
+        </>
+    );
+};
+
+export default Dashboard;
