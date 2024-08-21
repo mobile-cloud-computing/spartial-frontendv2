cbddc44bbeda926171761f8723e208f6e9bc9945
elemson <toluwanielemosho@gmail.com>
2024-08-21 17:06:16 +0300

    fix(auth): ensure that the application handles the redirectUri correctly

--- src/config/oktaConfig.ts
+++ src/config/oktaConfig.ts
@@ -1,8 +1,5 @@
-
-// const isLocalhost = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1';
-
 const redirectUri = `${window.location.origin}/login/callback`;
-// Okta configuration object
+
 export const oktaConfig = {
     clientId: process.env.REACT_APP_OKTA_CLIENT_ID || '',
     issuer: process.env.REACT_APP_OKTA_ISSUER || '',
