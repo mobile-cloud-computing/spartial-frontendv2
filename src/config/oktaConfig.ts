d856d861518a0f2663feecf2db617ed7aede4156
elemson <toluwanielemosho@gmail.com>
2024-08-21 17:28:33 +0300

    fix(auth): update okta redirect URI to use env values

--- src/config/oktaConfig.ts
+++ src/config/oktaConfig.ts
@@ -3,7 +3,7 @@ const redirectUri = `${window.location.origin}/login/callback`;
 export const oktaConfig = {
     clientId: process.env.REACT_APP_OKTA_CLIENT_ID || '',
     issuer: process.env.REACT_APP_OKTA_ISSUER || '',
-    redirectUri: redirectUri,
+    redirectUri: (process.env.REACT_APP_OKTA_REDIRECT_URI || redirectUri),
     scopes: (process.env.REACT_APP_OKTA_SCOPES || 'openid profile email').split(' '),
     pkce: process.env.REACT_APP_OKTA_PKCE === 'true',
     disableHttpsCheck: process.env.REACT_APP_OKTA_DISABLE_HTTPS_CHECK === 'true',
