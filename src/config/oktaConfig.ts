923f02f6975dbf3efec7d3bf507e9998500d7fe9
elemson <toluwanielemosho@gmail.com>
2024-09-11 13:08:36 +0300

    Update okta config to fix login issues in server

--- src/config/oktaConfig.ts
+++ src/config/oktaConfig.ts
@@ -1,8 +1,8 @@
 export const oktaConfig = {
     clientId: process.env.REACT_APP_OKTA_CLIENT_ID || '',
     issuer: process.env.REACT_APP_OKTA_ISSUER || '',
-    redirectUri: process.env.REACT_APP_OKTA_REDIRECT_URI || `${window.location.origin}/login/callback`,
-    scopes: (process.env.REACT_APP_OKTA_SCOPES || 'openid profile email').split(' '),
+    redirectUri: `${window.location.origin}/login/callback`,
+    scopes: ('openid profile email').split(' '),
     pkce: process.env.REACT_APP_OKTA_PKCE === 'true',
     disableHttpsCheck: process.env.REACT_APP_OKTA_DISABLE_HTTPS_CHECK === 'true',
 };
