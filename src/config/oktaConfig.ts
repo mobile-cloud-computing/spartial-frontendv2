befaddbc27b01f0f8a2c68998a516f99c33904b4
elemson <toluwanielemosho@gmail.com>
2024-09-11 16:55:38 +0300

    set ENV REACT_APP_OKTA_PKCE=true to false to fix login issues in live server

--- src/config/oktaConfig.ts
+++ src/config/oktaConfig.ts
@@ -2,7 +2,8 @@ export const oktaConfig = {
     clientId: process.env.REACT_APP_OKTA_CLIENT_ID || '',
     issuer: process.env.REACT_APP_OKTA_ISSUER || '',
     redirectUri: `${window.location.origin}/login/callback`,
-    scopes: (process.env.REACT_APP_OKTA_SCOPES || 'openid profile email').split(' '),
-    pkce: process.env.REACT_APP_OKTA_PKCE === 'true',
+    scopes: ('openid profile email').split(' '),
+    pkce: process.env.REACT_APP_OKTA_PKCE === 'false',
     disableHttpsCheck: process.env.REACT_APP_OKTA_DISABLE_HTTPS_CHECK === 'true',
+    devMode: true
 };
