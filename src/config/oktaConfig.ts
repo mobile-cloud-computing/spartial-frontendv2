9c82fdc778520cc0f1a1797e558ec35789b5d64e
elemson <toluwanielemosho@gmail.com>
2024-09-11 14:24:09 +0300

    set redirectUri to live server

--- src/config/oktaConfig.ts
+++ src/config/oktaConfig.ts
@@ -1,7 +1,7 @@
 export const oktaConfig = {
     clientId: process.env.REACT_APP_OKTA_CLIENT_ID || '',
     issuer: process.env.REACT_APP_OKTA_ISSUER || '',
-    redirectUri: `${window.location.origin}/login/callback`,
+    redirectUri: `http://193.40.154.121:3000/login/callback` || `${window.location.origin}/login/callback`,
     scopes: ('openid profile email').split(' '),
     pkce: process.env.REACT_APP_OKTA_PKCE === 'true',
     disableHttpsCheck: process.env.REACT_APP_OKTA_DISABLE_HTTPS_CHECK === 'true',
