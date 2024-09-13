208002ec5c3833b786fdaf05f128f671723d578c
elemson <toluwanielemosho@gmail.com>
2024-09-13 12:35:04 +0300

    Update redirect uri back to generic uri

--- src/config/oktaConfig.ts
+++ src/config/oktaConfig.ts
@@ -4,7 +4,7 @@ import { OktaAuthIdxOptions } from '@okta/okta-auth-js';
 export const oktaConfig: OktaAuthIdxOptions = {
     clientId: process.env.REACT_APP_OKTA_CLIENT_ID || '',
     issuer: process.env.REACT_APP_OKTA_ISSUER || '',
-    redirectUri: `http://193.40.154.121:3000/login/callback`,
+    redirectUri: `${window.location.origin}/login/callback`,
     scopes: ['openid', 'profile', 'email'],
     pkce: true,
     devMode: true
